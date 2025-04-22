ebedf8f8d3d528ff171e0d102ec8e1e3200909b6
Jesus Ballester <jesusmaballester@gmail.com>
2025-04-22 09:16:20 -0400

    Arreglo Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
     {{-- Enlace DataTable --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('css/datatables.min.css')}}"/>
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
     <link rel="stylesheet" href="{{ asset('fontawesome/css/all.css')}}">
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
