f08c3d4d6731deebc9a9088a1ff616c02b7489f1
Jesus Ballester <jesusmaballester@gmail.com>
2025-04-22 10:41:33 -0400

    cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,10 @@
     {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css')}}"> <!--Estilos--> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Jesusball14/mi-css@latest/app.css">
 </head>
 <body>
     
