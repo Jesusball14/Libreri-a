19aca2ca103d85361f678523a43f4d5146cce63c
Jesus Ballester <jesusmaballester@gmail.com>
2025-05-05 11:58:26 -0400

    arreglo img203

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -21,6 +21,9 @@ class AuthorController extends Controller
     }
 
     function store(Request $request){
+
+        dd($request->all(), $request->hasFile('image'), $request->file('image'));
+
         $request->validate([
             'name' => 'required|string|max:121',
             'lastname' => 'required|string|max:121',
